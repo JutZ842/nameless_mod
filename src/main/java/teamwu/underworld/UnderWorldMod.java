dc97a7dcd05915be5c95ae9a27fd3e4088b2469b
Wunst <matthiesbe@gmail.com>
2022-10-24 21:20:08 +0200

    Setup Mod ID

---
+++ src/main/java/teamwu/underworld/UnderWorldMod.java
@@ -0,0 +1,29 @@
+package teamwu.underworld;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod("underworld")
+public class UnderWorldMod {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public UnderWorldMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Hello from CommonSetup");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+}
