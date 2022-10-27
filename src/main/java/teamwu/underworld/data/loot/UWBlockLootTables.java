b43520f4a6dca8a0ede147ed8ebaa71bd4dd0a36
JutZ842 <75487339+JutZ842@users.noreply.github.com>
2022-10-27 18:57:50 +0200

    helper method for silktouch

--- src/main/java/teamwu/underworld/data/loot/UWBlockLootTables.java
+++ src/main/java/teamwu/underworld/data/loot/UWBlockLootTables.java
@@ -1,6 +1,7 @@
 package teamwu.underworld.data.loot;
 
 import net.minecraft.data.loot.BlockLoot;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.NotNull;
@@ -10,11 +11,16 @@ public class UWBlockLootTables extends BlockLoot {
     @Override
     protected void addTables() {
         dropSelf(UWBlocks.GNEISS_STONE.get());
-        add(UWBlocks.HORNFELS_STONE.get(), block -> createSingleItemTableWithSilkTouch(block, UWBlocks.HORNFELS_COBBLE.get()));
         dropSelf(UWBlocks.GREENSCHIST_STONE.get());
         dropSelf(UWBlocks.HORNFELS_STONE_WHITE.get());
         dropSelf(UWBlocks.HORNFELS_STONE_GRAY.get());
         dropSelf(UWBlocks.HORNFELS_COBBLE.get());
+
+        dropOtherWithoutSilktouch(UWBlocks.HORNFELS_STONE.get(), UWBlocks.HORNFELS_COBBLE.get());
+    }
+
+    private void dropOtherWithoutSilktouch(Block block, ItemLike notSilkedTouched) {
+        add(block, (var) -> createSingleItemTableWithSilkTouch(var, notSilkedTouched));
     }
 
     @Override
